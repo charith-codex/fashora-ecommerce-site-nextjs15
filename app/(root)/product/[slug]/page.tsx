3c1ddcfdfed62ed594b8c92a1d0f459ed2446b97
Charith Wickramasinghe <pvmcharith@gmail.com>
2025-01-12 23:58:12 +0530

    create product details page

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,74 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { getProductBySlug } from '@/lib/actions/product.actions';
+import { notFound } from 'next/navigation';
+
+const ProductDetailsPage = async (props: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await props.params;
+
+  const product = await getProductBySlug(slug);
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <>
+      <section>
+        <div className="grid grid-cols-1 md:grid-cols-5">
+          {/* images column */}
+          <div className="col-span-2"></div>
+          {/* details column */}
+          <div className="col-span-2 p-5">
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} {product.category}
+              </p>
+              <h1 className="h3-bold">{product.name}</h1>
+              <p>
+                {product.rating} of {product.numReviews} Reviews
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+                <p className="w-36 text-center rounded-full bg-pink-100 text-pink-700 px-5 py-2 font-semibold">
+                  LKR {product.price}
+                </p>
+              </div>
+            </div>
+            <div className="mt-10">
+              <p className="font-semibold">Description</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
+          {/* action column */}
+          <Card>
+            <CardContent className="p-4">
+              <div className="mb-2 flex justify-between">
+                <div>Price</div>
+                <div>
+                  <p>LKR {product.price}</p>
+                </div>
+              </div>
+              <div className="mb-2 flex justify-between">
+                <div>Status</div>
+                {product.stock > 0 ? (
+                  <Badge variant="outline">In Stock</Badge>
+                ) : (
+                  <Badge variant="destructive">Out of Stock</Badge>
+                )}
+              </div>
+              {product.stock > 0 && (
+                <div className="flex-center">
+                  <Button className="w-full">Add to Cart</Button>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,21 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   });
 
   return convertToPlainObject(data);
+}
+
+// get single product from it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
