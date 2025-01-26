5b8a73a90189662bb7ce0cc731691fcb473ea3d6
Charith Wickramasinghe <pvmcharith@gmail.com>
2025-01-26 14:22:53 +0530

    cart session setup

--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import { prisma } from '@/db/prisma';
 import CredentialsProviders from 'next-auth/providers/credentials';
 import { compareSync } from 'bcrypt-ts-edge';
 import type { NextAuthConfig } from 'next-auth';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export const config = {
   pages: {
@@ -86,6 +88,32 @@ export const config = {
         }
       }
       return token;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    authorized({ request, auth }: any) {
+      // Check for cart cookie
+      if (!request.cookies.get('sessionCartId')) {
+        // Generate cart cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create a new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set the newly generated sessionCartId in the response cookies
+        response.cookies.set('sessionCartId', sessionCartId);
+
+        // Return the response with the sessionCartId set
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
