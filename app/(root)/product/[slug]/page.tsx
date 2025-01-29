a64e76514d6573bea88f2a8639a23d380c81de6e
Charith Wickramasinghe <pvmcharith@gmail.com>
2025-01-29 19:21:06 +0530

    add remove cart action and dynamic button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { notFound } from 'next/navigation';
 import AddToCart from '@/components/shared/product/add-to-cart';
+import { getMyCart } from '@/lib/actions/cart.actions';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -14,6 +15,8 @@ const ProductDetailsPage = async (props: {
   if (!product) {
     notFound();
   }
+
+  const cart = await getMyCart();
 
   return (
     <>
@@ -64,6 +67,7 @@ const ProductDetailsPage = async (props: {
               {product.stock > 0 && (
                 <div className="flex-center">
                   <AddToCart
+                    cart={cart}
                     item={{
                       productId: product.id,
                       name: product.name,
